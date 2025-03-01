e586edb6943fa8d56e8bf006fa25c298e6d907cb
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-01 11:30:49 +0000

    Rules for variables

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ console.log('Hello World');
 
 let name = 'Joe';
 console.log(name);
+
+// cannot be reserved keywords, such as let, if ETC (variable name)
+// should be meaningful
+// cannot start with a number
+// cannot contain a space or a hyphen (-)
+// example
+// let Firstname (Camel notation in Java)
+
+//case sensitive 
+
+//declaring multiple variable 
+//let firstname, surname;
+//best one is declaring each variable on a single line.
