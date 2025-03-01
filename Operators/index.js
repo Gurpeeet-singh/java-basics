9408efe0d83f4f9ccf647e1d8d8f448899b16a32
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-01 13:30:58 +0000

    comparison

--- Operators/index.js
+++ Operators/index.js
@@ -2,18 +2,18 @@
 //arithmetic
 let x = 10;
 let y = 9;
-console.log(x + y); //addition
-console.log(x - y); //subtraction
-console.log(x * y); //Multiplication
-console.log(x / y); //division
-console.log(x % y); //remainder (MOD in python)
-console.log(x ** y); // exponential (x to the power of y)
-//increment
-console.log(++x); //icrement first then itll show
-console.log(x++); //shows the intial value and then does the icrement
-//decrement
-console.log(--x); //decrement first then itll show
-console.log(x--); //decrement the intial value and then does the icrement
+// console.log(x + y); //addition
+// console.log(x - y); //subtraction
+// console.log(x * y); //Multiplication
+// console.log(x / y); //division
+// console.log(x % y); //remainder (MOD in python)
+// console.log(x ** y); // exponential (x to the power of y)
+// //increment
+// console.log(++x); //icrement first then itll show
+// console.log(x++); //shows the intial value and then does the icrement
+// //decrement
+// console.log(--x); //decrement first then itll show
+// console.log(x--); //decrement the intial value and then does the icrement
 
 //assignment 
 
@@ -21,8 +21,14 @@ console.log(x--); //decrement the intial value and then does the icrement
 // x = x + 5 is equal to x +=5
 // all of the arithemitc has this combination such as, (*=, /=, ETC)
 
-
-
 //comparison
+//relational
+console.log(x > 0);
+console.log(x >= 0);
+console.log(x <= 0);
+
+//equality
+console.log(x === 0); //if it equal to it
+console.log(x !== 0); //check if it isnt equal 
 //logical
 //bitwise
