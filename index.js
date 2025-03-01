454f58a5b27ca1c4bbdbab96fe16fdb30a122b07
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-01 11:34:14 +0000

    how constants work

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ console.log(name);
 //declaring multiple variable 
 //let firstname, surname;
 //best one is declaring each variable on a single line.
+
+//constanst
+const InterestRate = 0.3
+console.log(InterestRate)
+
+//if you need to reassign use let 
+// if you do not need to reassign use const
