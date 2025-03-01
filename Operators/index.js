32fdb06c0aa3a2c5f33642b8df81c37f2c4a49a9
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-01 13:25:12 +0000

    arithemtic

--- Operators/index.js
+++ Operators/index.js
@@ -1,5 +1,24 @@
 //different types of operatos
 //arithmetic
+let x = 10;
+let y = 9;
+console.log(x + y); //addition
+console.log(x - y); //subtraction
+console.log(x * y); //Multiplication
+console.log(x / y); //division
+console.log(x % y); //remainder (MOD in python)
+console.log(x ** y); // exponential (x to the power of y)
+//increment
+console.log(++x); //icrement first then itll show
+console.log(x++); //shows the intial value and then does the icrement
+//decrement
+console.log(--x); //decrement first then itll show
+console.log(x--); //decrement the intial value and then does the icrement
+
+
+
+
+
 //assignment 
 //comparison
 //logical
