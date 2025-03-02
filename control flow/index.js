310ef0d8d9d5db048138fc80993b4605aa46380b
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-02 09:38:47 +0000

    if...else statements

---
+++ control flow/index.js
@@ -0,0 +1,40 @@
+//conditional statements
+// we have 2 
+//if..else
+
+//if hour is between 6am to 6pm; good morning 
+//if it is between 12pm and 6pm: good evening 
+//otherwise: goodevening
+
+// if (condition) {
+//      statement
+// }
+
+//else if (coniditon) {
+//     statment
+// }
+
+//no limit......    
+
+//if none of these conditions are met you can use
+
+
+//else
+    // statment
+
+
+let hour = 17;
+
+if ( hour >= 6 && hour <12) 
+    console.log('good morning')
+
+else if ( hour >=12 && hour <18) 
+    console.log("good afternoon")
+
+else 
+    console.log("good evening")
+
+
+//you dont need the {} 
+
+//switch..case
