d1a50149e614a658ee70d7e718f7400985df53b0
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-02 09:46:27 +0000

    switch case

--- control flow/index.js
+++ control flow/index.js
@@ -20,9 +20,7 @@
 
 
 //else
-    // statment
-
-
+// / statment
 let hour = 17;
 
 if ( hour >= 6 && hour <12) 
@@ -38,3 +36,45 @@ else
 //you dont need the {} 
 
 //switch..case
+
+//let role;
+//switch (variable, this case role) {
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+// }
+//scanrios, seeing what permision someone has
+
+let role = 'Guest';
+
+switch (role) {
+    case 'Guest':
+        console.log('Guest user');
+        break; //YOU NEED THIS HERE
+    case 'Moderator':
+        console.log("Moderator");
+        break;
+    case 'Admin':
+        console.log("Admin User");
+        break;
+    default:
+        console.log("Unknown role")
+}
+
+//this can be used in if
+
+if (role === 'Guest') {
+    console.log('Guest user using the if loop')
+}
+else {
+    console.log("Unknown users")
+}
+
+//if is cleaner, but its up to you
+
+
+
+
+
+//this is just the top one
+
