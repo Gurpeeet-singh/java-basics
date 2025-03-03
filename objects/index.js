3c666ec7a283cb211c205cf1071c770740a76b26
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-03 16:24:23 +0000

    factory functions

--- objects/index.js
+++ objects/index.js
@@ -14,16 +14,42 @@
 // }
 // you can have a obj in a obj
 //obj orientated style programming (OOP)
-const circle = {
-    radius: 1, 
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function() {
-        console.log('wsg');
-    }
-}; //can store anything u want
+// const circle = {
+//     radius: 1, 
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+//     draw: function() {
+//         console.log('wsg');
+//     }
+// }; //can store anything u want
 
-circle.draw();
+//how to create a factory function
+// function createricle(radius, location) {
+//      return {
+//         radius: radius, 
+//         location: location, //see this way is a lot easier 
+//         isVisible: true,
+//         draw: function() {
+//             console.log('wsg');
+//         }
+//     }; //can store anything u want   
+// }
+function createricle(radius) {
+    return {
+       radius, //if they key and the paramter name are the same you can just do this
+    //    draw: function() {
+    //        console.log('wsg');
+    //    } 
+    //      instead of doind this for the function you can do it like below
+        draw(){
+            console.log('draw')
+        }
+   }; //can store anything u want   
+}
+const circle1 = createricle(1);
+console.log(circle1);
+const circle2 = createricle(2);
+console.log(circle2);
