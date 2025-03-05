bc6fa9b4c9b3afe70cb0e21841e69fa2143518c7
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-05 20:52:55 +0000

    blog post object

---
+++ objects/blog post object/index.js
@@ -0,0 +1,23 @@
+//create a blog post object with these properties
+// title
+// body 
+// auhtor 
+// views 
+// comments 
+// each comment sould have author and body 
+// islive 
+
+const blog = {
+    title: 'JOE',
+    body: 'YK BACK IN THE DAY',
+    author: 'Joeee',
+    views: 100,
+    comments: [
+        {author: 'JOE', comment:'AMAZING'},
+        {author: 'JOE', comment:'AMAZING'},
+        {author: 'JOE', comment:'AMAZING'},
+        {author: 'JOE', comment:'AMAZING'},
+    ],
+    islive: true
+};
+console.log(blog);
