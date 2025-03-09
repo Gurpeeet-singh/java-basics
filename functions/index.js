8ad9fc40342b985f6e8f31795b570c9f800f9f96
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-09 16:44:46 +0000

    1- Function Declarations vs Expressions

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+// Function Declarations vs Expressions
+//funtion declaration
+function walk() {
+    console.log('walk');
+} // dont really put on here 
+ 
+
+// anonymous function expresssion 
+let run = function() {
+    console.log("wsg")
+}; //; here 
+
+//not anonymouse bc it has a name
+let run1 = function walk() {
+    console.log("wsg")
+}; //; here 
