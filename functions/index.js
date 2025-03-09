8d4e0368c75e540494e26ea8d4fe0e9d789e7dcb
Gurpreet Singh <gurpreetsingh.19842008@gmail.com>
2025-03-09 16:54:03 +0000

    arguments

--- functions/index.js
+++ functions/index.js
@@ -20,3 +20,26 @@ let run1 = function walk() {
 // you cant for a function expression 
 
 //hoisting defintion: the process of moving function declarations to the top of the file. This is done automatically done by the javascript engine upon executing the code
+
+
+//arguments 
+// function sum(a, b) {
+//     return a + b
+// }
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+// youll get 3
+
+// you dont need to have all parameters filled, if you dont youll get NaN in this scenario. you can also
+// add as many parameters you want, it doesnt matter but the first 2 will get added up in this scenario
+
+// how to make a flexable function 
+
+function sum() {
+    let total = 0;
+    for(let value of arguments) {
+        total =+ value;
+    }
+    return total;
+}
+// would return whatever them numbers are added up 
